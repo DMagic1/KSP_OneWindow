25cb3282030cdd56e95a47e0d8defa88504eac3c
DMagic <david.grandy@gmail.com>
2017-08-14 13:40:31 -0400

    Unity assets

---
+++ Source/Unity/One Screen/Assets/Bundler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+public class Bundler
+{
+	const string dir = "AssetBundles";
+	const string extension = "";
+
+    [MenuItem("One Window/Build Bundle")]
+    static void BuildAllAssetBundles()
+    {		
+		BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.ForceRebuildAssetBundle, BuildTarget.StandaloneWindows);
+	}
+
+
+}
